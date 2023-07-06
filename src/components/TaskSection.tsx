9fbd60a1c92a2ae2af1a469d60343d23c214b314
김준섭 <junsobi@gmail.com>
2023-07-06 19:16:14 +0900

    Modify : 폴더구조 변경

--- src/components/TaskSection.tsx
+++ src/components/TaskSection.tsx
@@ -2,34 +2,31 @@ import React from 'react';
 import Checkbox from './Checkbox';
 import Button from './Button';
 
-export type ToDoItemProps = {
-  text: string;
-  completed: boolean;
-};
-
-const ToDoItem = ({ text, completed }: ToDoItemProps) => (
-  <li className="border-b border-gray-200 p-2 flex justify-between items-center gap-4 hover:bg-gray-300">
-    <div className="flex gap-4">
-      <Checkbox className="task-check" checked={completed} />
-      <span className={completed ? 'w-full line-through' : 'w-full'}>
-        {text}
-      </span>
-    </div>
-    <Button className="delete-btn">🗑️</Button>
-  </li>
-);
-
-type TaskSectionProps = {
+type TaskProps = {
   title: string;
-  tasks: ToDoItemProps[];
+  tasks: {
+    text: string;
+    completed: boolean;
+  }[];
 };
 
-const TaskSection = ({ title, tasks }: TaskSectionProps) => (
+const TaskSection = ({ title, tasks }: TaskProps) => (
   <div className="task-container mb-5">
     <h2 className="text-2xl mb-3">{title}</h2>
     <ul>
       {tasks.map((task, i) => (
-        <ToDoItem key={i} {...task} />
+        <li
+          key={i}
+          className="border-b border-gray-200 p-2 flex justify-between items-center gap-4 hover:bg-gray-300"
+        >
+          <div className="flex gap-4">
+            <Checkbox className="task-check" checked={task.completed} />
+            <span className={task.completed ? 'w-full line-through' : 'w-full'}>
+              {task.text}
+            </span>
+          </div>
+          <Button className="delete-btn">🗑️</Button>
+        </li>
       ))}
     </ul>
   </div>
