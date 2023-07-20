3b9d9a817cfa051a4ee0bc5469191967284285a6
김준섭 <junsobi@gmail.com>
2023-07-20 17:20:51 +0900

    Remove all cached files

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
-
-const Button = ({ ...props }: ButtonProps) => {
-  return <button {...props}></button>;
-};
-
-export default Button;
--- src/components/Input.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
-
-const Input = ({ ...props }: InputProps) => {
-  return <input type="text" {...props} />;
-};
-
-export default Input;
